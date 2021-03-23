ebce883b35230f92e01c81be257b6a471211e7f5
徐侃锋 <xu_kanfeng@192.168.0.105>
2021-03-23 21:38:27 +0800

    add

--- 300.最长递增子序列.js
+++ 300.最长递增子序列.js
@@ -10,6 +10,18 @@
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
+  // 注意点：dp[i] 表示以 nums[i] 这个数结尾的最长递增子序列的长度
+  const dp = new Array(nums.length + 1).fill(1)
 
+  for (let i = 0; i < nums.length; i++) {
+    // 从 i 前面的数字开始找
+    for (let j = 0; j < i; j++) {
+      // 如果位置 j 的数字小于当前数字，就有可能产生新的最大长度。位置 j 的最大长度加 1，就是新的最大长度
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1)
+      }
+    }
+  }
+  return dp.reduce((a, b) => Math.max(a, b))
 };
 // @lc code=end
--- 678.有效的括号字符串.js
+++ 678.有效的括号字符串.js
@@ -10,6 +10,23 @@
  * @return {boolean}
  */
 var checkValidString = function (s) {
-
+  let left = [], star = []
+  for (let i = 0; i < s.length; i++){
+    if (s[i] === "(") left.push(i)
+    if (s[i] === "*") star.push(i)
+    if (s[i] === ")") {
+      if (left.length === 0) {
+        if (star.length === 0) return false
+        star.pop()
+      } else {
+        left.pop()
+      }
+    }
+  }
+  if (left.length > star.length) return false
+  while (left.length && star.length) {
+    if (left.pop() > star.pop()) return false
+  }
+  return true
 };
 // @lc code=end
